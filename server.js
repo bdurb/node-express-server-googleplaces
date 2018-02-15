0d2d90f81f8ca30e7f4d253da9e1ff1ef911c804
bdurb <brian@rpmgreenville.com>
2018-02-15 15:35:54 -0500

    server functional

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 
+const config = require('./config');
 const express = require('express');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+
 
 const server = express();
 const PORT = config.port;
 const USER_ERROR = 422;
 
 server.use(bodyParser.json());
+
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+});
