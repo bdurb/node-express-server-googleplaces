b7ef09d5be42bbad4bdc442a6e12d814105870e0
bdurb <brian@rpmgreenville.com>
2018-02-15 17:10:57 -0500

    setting up post route with fetch

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ const USER_ERROR = 422;
 
 server.use(bodyParser.json());
 
+server.post('/places', (req, res) => {
+  const place = req.body.place;
+  if (!place) {
+    res.send(USER_ERROR);
+    res.json({ error: 'You must provide a place' });
+  }
+  let data = fetch(`https://maps.googleapis.com/maps/api/place/textsearch/xml?query=${place}&key=${config.gmaps.apiKey}`,{ method: 'POST'})
+    .then(res => res.json(data))
+    .then(json => {
+      res.status(200).json(first)
+    });
+});
+
+
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
